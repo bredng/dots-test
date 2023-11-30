faab2f8d80e344b43c95d843fd8eb6d02743f1c3
bdan0005 <bdan0005@student.monash.edu>
2023-12-01 10:45:20 +1100

    Added ROI selection and new file to store helper functions

---
+++ helpers.py
@@ -0,0 +1,6 @@
+def calculate_ave_v(x, x0, t):
+    return (x-x0)/t
+
+def get_subimage(img, x, y, width, height):
+    return img[y:y+height, x:x+width]
+
--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 import numpy as np
-import cv2
+import cv2 
+import time
+from helpers import *
 
-cap = cv2.VideoCapture("testvid.mp4")
-
-ret, frame1 = cap.read()
 
 # Setting parameters
-winSize = cv2.Size(15, 15)
-maxLevel = 2
-maxCorners = 30
-qualityLevel = 0.3
-minDistance = 7
-blockSize = 7
+lk_params = dict(winSize = (15, 15),
+                 maxLevel = 2,
+                 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.001))
+
+feature_params = dict(maxCorners = 20, 
+                      qualityLevel = 0.3, 
+                      minDistance = 7, blockSize = 7)
+
+# Array to store all the calculated velocities
+velocities = []
+
+# Frame capture
+cap = cv2.VideoCapture("testvid.mp4")
+
+succ, first_frame = cap.read()
+
+# Prompt user to select region of interest
+x, y, w, h = cv2.selectROI("Select region of interest", first_frame, True, False)
+
+ROI_img = get_subimage(first_frame, x, y, w, h)
+
+# Find centroid of object in region of interest
+gray_ROI = cv2.cvtColor(ROI_img, cv2.COLOR_BGR2GRAY)
+
+succ, thres = cv2.threshold(gray_ROI, 127, 255, cv2.THRESH_BINARY)
+
+M = cv2.moments(thres)
+
+# # Convert the first frame to grayscale
+# previous = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+# p0 = cv2.goodFeaturesToTrack(previous, mask = None, **feature_params)
+
+# while True:
+#     frame_start = time.time()
+#     succ, second_frame = cap.read()
+#     next = cv2.cvtColor(second_frame, cv2.COLOR_BGR2GRAY)
+
+#     pts0, st, err = cv2.calcOpticalFlowPyrLK(previous, next, )
+
+#     if not succ:
+#         print("Couldn't retrieve frame, ending...")
+#         break
+    
+#     frame_end = time.time()
+
 
 
 
